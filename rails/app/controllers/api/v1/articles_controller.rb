97780d2ce8cd9c0b39cde54c2afa405bb58eaca7
s-ko <song.1209@icloud.com>
2024-04-12 00:02:25 +0900

    fix: n+1

--- rails/app/controllers/api/v1/articles_controller.rb
+++ rails/app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ArticlesController < ApplicationController
   def index
-    articles = Article.published.order_by_newest.page(params[:page] || 1).per(10)
+    articles = Article.preload(:user).published.order_by_newest.page(params[:page] || 1).per(10)
     render json: articles
   end
 
